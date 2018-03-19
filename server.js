eb2b6316ee85c8fef3581a6e2f1b75377f66b0bd
branch1user1 <branch1user1@gmail.com>
2018-03-19 08:17:55 -0400

    second

--- server.js
+++ server.js
@@ -1,4 +1,7 @@
+var http = require("http");
+
 http.createServer(function (request, response) {
+
    // Send the HTTP header 
    // HTTP Status: 200 : OK
    // Content Type: text/plain
@@ -6,7 +9,7 @@ http.createServer(function (request, response) {
    
    // Send the response body as "Hello World"
    response.end('Hello World\n');
-}).listen(8081);
+}).listen(3000);
 
 // Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+console.log('Server running at http://127.0.0.1:8081/')
